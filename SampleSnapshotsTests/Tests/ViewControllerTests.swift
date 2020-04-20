fa2b1162e1410564fa984e1f43e284ebb1286b01
Tatsunori Kuri <kuritatu18@gmail.com>
2020-04-20 13:58:24 +0900

    add snapshots test case

---
+++ SampleSnapshotsTests/Tests/ViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerTests.swift
+//  SampleSnapshotsTests
+//
+//  Created by Tatsunori Kuri on 2020/04/20.
+//  Copyright © 2020 kuritatu18. All rights reserved.
+//
+
+import XCTest
+@testable import SampleSnapshots
+import FBSnapshotTestCase
+
+class DashboardViewControllerTests: SnapshotTestCase {
+
+    func testDashboardSnapshot() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ViewController")
+        FBSnapshotVerifyView(vc.view)
+    }
+}
