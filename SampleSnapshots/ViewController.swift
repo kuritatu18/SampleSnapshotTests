dd5ed9467b589c6faefdd5dfda2211c652782f96
Tatsunori Kuri <kuritatu18@gmail.com>
2020-04-20 13:34:33 +0900

    add project

---
+++ SampleSnapshots/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSnapshots
+//
+//  Created by Tatsunori Kuri on 2020/04/20.
+//  Copyright © 2020 kuritatu18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
