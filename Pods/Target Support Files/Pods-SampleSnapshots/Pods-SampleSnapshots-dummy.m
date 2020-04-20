5cc43a4cc62e140f72e21ab11482442c211b2853
Tatsunori Kuri <kuritatu18@gmail.com>
2020-04-20 14:31:58 +0900

    add gitignore to Pods

--- Pods/Target Support Files/Pods-SampleSnapshots/Pods-SampleSnapshots-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleSnapshots : NSObject
-@end
-@implementation PodsDummy_Pods_SampleSnapshots
-@end
--- Pods/Target Support Files/Pods-SampleSnapshotsTests/Pods-SampleSnapshotsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleSnapshotsTests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleSnapshotsTests
-@end
